63e81f027afe9e72cde124318b8c2ca535776efa
Syzygies <davebayer@mac.com>
2020-03-26 10:25:29 -0700

    update config.h

---
+++ keyboards/dz60/keymaps/syzygies/config.h
@@ -0,0 +1,8 @@
+// Copyright 2020 Dave Bayer davebayer@mac.com @Syzygies <https://unlicense.org>
+
+#pragma once
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
+#define RETRO_TAPPING
+#define IGNORE_MOD_TAP_INTERRUPT
