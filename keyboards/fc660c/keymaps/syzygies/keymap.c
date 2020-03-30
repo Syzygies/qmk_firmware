1a1f665a09283c6c7939cc817a21c08cc3b509d8
Syzygies <davebayer@mac.com>
2020-03-30 02:19:05 -0700

    debugging

--- keyboards/fc660c/keymaps/syzygies/keymap.c
+++ keyboards/fc660c/keymaps/syzygies/keymap.c
@@ -3,7 +3,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+<<<<<<< Updated upstream
   // Safety
+=======
+  // Safety       
+>>>>>>> Stashed changes
   [0] = LAYOUT(
     KC_GRV  , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_6    , KC_7          , KC_8            , KC_9           , KC_0       , KC_MINS     , KC_EQL    , LT(2,KC_BSPC) , DF(1)   ,
     KC_TAB  , KC_Q       , KC_W         , KC_E         , KC_R         , KC_T    , KC_Y    , KC_U          , KC_I            , KC_O           , KC_P       , KC_LBRC     , KC_RBRC   , KC_BSLS       , KC_DEL  ,
@@ -14,14 +18,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Base
   [1] = LAYOUT(
     KC_GRV  , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_6    , KC_7          , KC_8            , KC_9           , KC_0       , KC_MINS     , KC_EQL    , LT(2,KC_BSPC) , XXXXXXX ,
+<<<<<<< Updated upstream
     KC_TAB  , KC_Q       , KC_W         , LT(4,KC_E)   , LT(3,KC_R)   , KC_T    , KC_Y    , LT(3,KC_U)    , LT(4,KC_I)      , KC_O           , KC_P       , KC_LBRC     , KC_RBRC   , KC_BSLS       , KC_DEL  ,
     KC_ESC  , KC_A       , LSFT_T(KC_S) , LT(6,KC_D)   , LT(5,KC_F)   , KC_G    , KC_H    , LT(5,KC_J)    , LT(6,KC_K)      , RSFT_T(KC_L)   , KC_SCLN    , KC_QUOT     , KC_ENT    ,
+=======
+    KC_TAB  , KC_Q       , LT(5,KC_W)   , LT(4,KC_E)   , LT(3,KC_R)   , KC_T    , KC_Y    , LT(3,KC_U)    , LT(4,KC_I)      , LT(5,KC_O)     , KC_P       , KC_LBRC     , KC_RBRC   , KC_BSLS       , KC_DEL  ,
+    KC_ESC  , KC_A       , LSFT_T(KC_S) , LT(7,KC_D)   , LT(6,KC_F)   , KC_G    , KC_H    , LT(8,KC_J)    , LT(9,KC_K)      , RSFT_T(KC_L)   , KC_SCLN    , KC_QUOT     , KC_ENT    ,
+>>>>>>> Stashed changes
     KC_LSFT , KC_Z       , LCTL_T(KC_X) , LALT_T(KC_C) , LGUI_T(KC_V) , KC_B    , KC_N    , RGUI_T(KC_M)  , RALT_T(KC_COMM) , RCTL_T(KC_DOT) , KC_SLSH    , KC_RSFT     , KC_UP     ,
     KC_LCTL , KC_LALT    , KC_LGUI      , KC_SPC       , KC_RGUI      , KC_RALT , KC_RCTL , KC_LEFT       , KC_DOWN         , KC_RGHT        
   ),
   // Backspace
   [2] = LAYOUT(
+<<<<<<< Updated upstream
     XXXXXXX , KC_F1      , KC_F2        , KC_F3        , KC_F4        , KC_F5   , KC_F6   , KC_F7         , KC_F8           , KC_F9          , KC__MUTE   , KC__VOLDOWN , KC__VOLUP , XXXXXXX       , DF(1)   ,
+=======
+    XXXXXXX , KC_F1      , KC_F2        , KC_F3        , KC_F4        , KC_F5   , KC_F6   , KC_F7         , KC_F8           , KC_F9          , KC__MUTE   , KC__VOLDOWN , KC__VOLUP , _______       , XXXXXXX ,
+>>>>>>> Stashed changes
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
@@ -29,7 +42,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // RU
   [3] = LAYOUT(
+<<<<<<< Updated upstream
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , DF(1)   ,
+=======
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
+>>>>>>> Stashed changes
     XXXXXXX , MEH(KC_Q)  , MEH(KC_W)    , MEH(KC_E)    , MEH(KC_R)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
     XXXXXXX , MEH(KC_A)  , MEH(KC_S)    , MEH(KC_D)    , MEH(KC_F)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , MEH(KC_Z)  , MEH(KC_X)    , MEH(KC_C)    , MEH(KC_V)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
@@ -37,25 +54,68 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // EI
   [4] = LAYOUT(
+<<<<<<< Updated upstream
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , DF(1)   ,
+=======
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
+>>>>>>> Stashed changes
     XXXXXXX , HYPR(KC_Q) , HYPR(KC_W)   , HYPR(KC_E)   , HYPR(KC_R)   , XXXXXXX , XXXXXXX , S(G(KC_RBRC)) , S(G(KC_LBRC))   , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
     XXXXXXX , HYPR(KC_A) , HYPR(KC_S)   , HYPR(KC_D)   , HYPR(KC_F)   , XXXXXXX , XXXXXXX , SCMD(KC_B)    , G(KC_P)         , G(KC_F)        , A(G(KC_F)) , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , HYPR(KC_Z) , HYPR(KC_X)   , HYPR(KC_C)   , HYPR(KC_V)   , XXXXXXX , XXXXXXX , G(KC_G)       , S(G(KC_G))      , S(G(KC_F))     , S(G(KC_H)) , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
   ),
+<<<<<<< Updated upstream
   // FJ
   [5] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , DF(1)   ,
     KC_TAB  , KC_EXLM    , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC , KC_CIRC , KC_AMPR       , KC_ASTR         , KC_LPRN        , KC_RPRN    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
     KC_ESC  , KC_TILD    , KC_LCBR      , KC_RCBR      , KC_DQUO      , KC_PLUS , KC_EQL  , KC_QUOT       , KC_LBRC         , KC_RBRC        , KC_COLN    , XXXXXXX     , KC_ENT    ,
+=======
+  // WO
+  [5] = LAYOUT(
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , _______        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , XXXXXXX ,
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
+    XXXXXXX , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_6    , KC_7          , KC_8            , KC_9           , KC_0       , XXXXXXX     , XXXXXXX   ,
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
+  ),
+  // FJ
+  [6] = LAYOUT(
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , XXXXXXX ,
+    KC_TAB  , KC_EXLM    , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC , KC_CIRC , KC_AMPR       , KC_ASTR         , KC_LPRN        , KC_RPRN    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
+    KC_ESC  , KC_TILD    , KC_LCBR      , KC_RCBR      , _______      , KC_PLUS , KC_EQL  , KC_QUOT       , KC_LBRC         , KC_RBRC        , KC_COLN    , XXXXXXX     , KC_ENT    ,
+>>>>>>> Stashed changes
     XXXXXXX , KC_GRV     , KC_PIPE      , KC_BSLS      , KC_UNDS      , KC_BSPC , KC_DEL  , KC_MINS       , KC_LT           , KC_GT          , KC_QUES    , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , XXXXXXX    , XXXXXXX      , KC_SPC       , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
   ),
   // DK
+<<<<<<< Updated upstream
   [6] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , DF(1)   ,
     KC_TAB  , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
     KC_ESC  , KC_6       , KC_7         , KC_8         , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , KC_UP           , KC_RGHT        , XXXXXXX    , XXXXXXX     , KC_ENT    ,
+=======
+  [7] = LAYOUT(
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , XXXXXXX ,
+    KC_TAB  , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
+    KC_ESC  , KC_6       , KC_7         , _______      , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , KC_UP           , KC_RGHT        , XXXXXXX    , XXXXXXX     , KC_ENT    ,
+    XXXXXXX , KC_SLSH    , KC_DOT       , KC_COMM      , KC_MINS      , KC_BSPC , KC_DEL  , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
+    XXXXXXX , XXXXXXX    , XXXXXXX      , KC_SPC       , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
+  ),
+  // FJ
+  [8] = LAYOUT(
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , XXXXXXX ,
+    KC_TAB  , KC_EXLM    , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC , KC_CIRC , KC_AMPR       , KC_ASTR         , KC_LPRN        , KC_RPRN    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
+    KC_ESC  , KC_TILD    , KC_LCBR      , KC_RCBR      , KC_DQUO      , KC_PLUS , KC_EQL  , _______       , KC_LBRC         , KC_RBRC        , KC_COLN    , XXXXXXX     , KC_ENT    ,
+    XXXXXXX , KC_GRV     , KC_PIPE      , KC_BSLS      , KC_UNDS      , KC_BSPC , KC_DEL  , KC_MINS       , KC_LT           , KC_GT          , KC_QUES    , XXXXXXX     , XXXXXXX   ,
+    XXXXXXX , XXXXXXX    , XXXXXXX      , KC_SPC       , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
+  ),
+  // DK
+  [9] = LAYOUT(
+    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , KC_BSPC       , XXXXXXX ,
+    KC_TAB  , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , KC_DEL  ,
+    KC_ESC  , KC_6       , KC_7         , KC_8         , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , _______         , KC_RGHT        , XXXXXXX    , XXXXXXX     , KC_ENT    ,
+>>>>>>> Stashed changes
     XXXXXXX , KC_SLSH    , KC_DOT       , KC_COMM      , KC_MINS      , KC_BSPC , KC_DEL  , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   ,
     XXXXXXX , XXXXXXX    , XXXXXXX      , KC_SPC       , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        
   )
