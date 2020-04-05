03f78eb73ee964b296fe563cffd60b141e7872e1
Syzygies <davebayer@mac.com>
2020-04-05 16:48:42 -0700

    isolated bug

--- keyboards/fc660c/keymaps/syzygies/keymap.c
+++ keyboards/fc660c/keymaps/syzygies/keymap.c
@@ -12,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT(
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        , _______    , _______     , _______   , LT(2,KC_BSPC) , _______ ,
-    _______ , _______    , LT(5,KC_W)   , LT(6,KC_E)   , LT(7,KC_R)   , _______ , _______ , LT(8,KC_U)    , LT(9,KC_I)      , LT(10,KC_O)    , _______    , _______     , _______   , _______       , _______ ,
-    _______ , _______    , LSFT_T(KC_S) , LT(11,KC_D)  , LT(12,KC_F)  , _______ , _______ , LT(13,KC_J)   , LT(14,KC_K)     , RSFT_T(KC_L)   , _______    , _______     , _______   ,
+    _______ , _______    , LT(7,KC_W)   , LT(6,KC_E)   , LT(5,KC_R)   , _______ , _______ , LT(5,KC_U)    , LT(6,KC_I)      , LT(7,KC_O)     , _______    , _______     , _______   , _______       , _______ ,
+    _______ , _______    , LSFT_T(KC_S) , LT(4,KC_D)   , LT(3,KC_F)   , _______ , _______ , LT(3,KC_J)    , LT(4,KC_K)      , RSFT_T(KC_L)   , _______    , _______     , _______   ,
     _______ , _______    , LCTL_T(KC_X) , LALT_T(KC_C) , LGUI_T(KC_V) , _______ , _______ , RGUI_T(KC_M)  , RALT_T(KC_COMM) , RCTL_T(KC_DOT) , _______    , _______     , _______   ,
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
   ),
@@ -34,78 +34,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [4] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
     _______ , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    TG(4)   , KC_6       , KC_7         , KC_8         , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , KC_UP           , KC_RGHT        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , KC_SLSH    , KC_DOT       , KC_COMM      , KC_MINS      , KC_BSPC , KC_DEL  , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
+    TG(4)   , KC_6       , KC_7         , KC_8         , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , KC_UP           , KC_RGHT        , KC_EQL     , XXXXXXX     , _______   ,
+    _______ , XXXXXXX    , KC_COLN      , KC_COMM      , KC_DOT       , KC_BSPC , KC_DEL  , KC_PLUS       , KC_MINS         , KC_ASTR        , KC_SLSH    , _______     , _______   ,
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
   ),
   [5] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , XXXXXXX    , _______      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
+    _______ , MEH(KC_Q)  , MEH(KC_W)    , MEH(KC_E)    , MEH(KC_R)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
+    TG(3)   , MEH(KC_A)  , MEH(KC_S)    , MEH(KC_D)    , MEH(KC_F)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , _______   ,
+    _______ , MEH(KC_Z)  , MEH(KC_X)    , MEH(KC_C)    , MEH(KC_V)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
   ),
   [6] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , HYPR(KC_Q) , HYPR(KC_W)   , _______      , HYPR(KC_R)   , XXXXXXX , XXXXXXX , S(G(KC_LBRC)) , S(G(KC_RBRC))   , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    _______ , HYPR(KC_A) , HYPR(KC_S)   , HYPR(KC_D)   , HYPR(KC_F)   , XXXXXXX , XXXXXXX , SCMD(KC_B)    , G(KC_P)         , G(KC_F)        , A(G(KC_F)) , XXXXXXX     , _______   ,
+    _______ , HYPR(KC_Q) , HYPR(KC_W)   , HYPR(KC_E)   , HYPR(KC_R)   , XXXXXXX , XXXXXXX , S(G(KC_LBRC)) , S(G(KC_RBRC))   , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
+    TG(4)   , HYPR(KC_A) , HYPR(KC_S)   , HYPR(KC_D)   , HYPR(KC_F)   , XXXXXXX , XXXXXXX , SCMD(KC_B)    , G(KC_P)         , G(KC_F)        , A(G(KC_F)) , XXXXXXX     , _______   ,
     _______ , HYPR(KC_Z) , HYPR(KC_X)   , HYPR(KC_C)   , HYPR(KC_V)   , XXXXXXX , XXXXXXX , S(G(KC_G))    , G(KC_G)         , S(G(KC_F))     , S(G(KC_H)) , _______     , _______   ,
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
   ),
   [7] = LAYOUT(
     XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , MEH(KC_Q)  , MEH(KC_W)    , MEH(KC_E)    , _______      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    _______ , MEH(KC_A)  , MEH(KC_S)    , MEH(KC_D)    , MEH(KC_F)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , MEH(KC_Z)  , MEH(KC_X)    , MEH(KC_C)    , MEH(KC_V)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [8] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , MEH(KC_Q)  , MEH(KC_W)    , MEH(KC_E)    , MEH(KC_R)    , XXXXXXX , XXXXXXX , _______       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    _______ , MEH(KC_A)  , MEH(KC_S)    , MEH(KC_D)    , MEH(KC_F)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , MEH(KC_Z)  , MEH(KC_X)    , MEH(KC_C)    , MEH(KC_V)    , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [9] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , HYPR(KC_Q) , HYPR(KC_W)   , HYPR(KC_E)   , HYPR(KC_R)   , XXXXXXX , XXXXXXX , S(G(KC_LBRC)) , _______         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    _______ , HYPR(KC_A) , HYPR(KC_S)   , HYPR(KC_D)   , HYPR(KC_F)   , XXXXXXX , XXXXXXX , SCMD(KC_B)    , G(KC_P)         , G(KC_F)        , A(G(KC_F)) , XXXXXXX     , _______   ,
-    _______ , HYPR(KC_Z) , HYPR(KC_X)   , HYPR(KC_C)   , HYPR(KC_V)   , XXXXXXX , XXXXXXX , S(G(KC_G))    , G(KC_G)         , S(G(KC_F))     , S(G(KC_H)) , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [10] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , _______        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
+    _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
     _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , _______   ,
     _______ , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [11] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    TG(4)   , KC_6       , KC_7         , _______      , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , KC_UP           , KC_RGHT        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , KC_SLSH    , KC_DOT       , KC_COMM      , KC_MINS      , KC_BSPC , KC_DEL  , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [12] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , KC_EXLM    , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC , KC_CIRC , KC_AMPR       , KC_ASTR         , KC_LPRN        , KC_RPRN    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    TG(3)   , KC_TILD    , KC_LCBR      , KC_RCBR      , _______      , KC_PLUS , KC_EQL  , KC_QUOT       , KC_LBRC         , KC_RBRC        , KC_COLN    , XXXXXXX     , _______   ,
-    _______ , KC_GRV     , KC_PIPE      , KC_BSLS      , KC_UNDS      , KC_BSPC , KC_DEL  , KC_MINS       , KC_LT           , KC_GT          , KC_QUES    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [13] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , KC_EXLM    , KC_AT        , KC_HASH      , KC_DLR       , KC_PERC , KC_CIRC , KC_AMPR       , KC_ASTR         , KC_LPRN        , KC_RPRN    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    TG(3)   , KC_TILD    , KC_LCBR      , KC_RCBR      , KC_DQUO      , KC_PLUS , KC_EQL  , _______       , KC_LBRC         , KC_RBRC        , KC_COLN    , XXXXXXX     , _______   ,
-    _______ , KC_GRV     , KC_PIPE      , KC_BSLS      , KC_UNDS      , KC_BSPC , KC_DEL  , KC_MINS       , KC_LT           , KC_GT          , KC_QUES    , _______     , _______   ,
-    _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
-  ),
-  [14] = LAYOUT(
-    XXXXXXX , XXXXXXX    , XXXXXXX      , XXXXXXX      , XXXXXXX      , XXXXXXX , XXXXXXX , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , XXXXXXX     , XXXXXXX   , _______       , _______ ,
-    _______ , KC_1       , KC_2         , KC_3         , KC_4         , KC_5    , KC_HOME , KC_PGDOWN     , KC_PGUP         , KC_END         , XXXXXXX    , XXXXXXX     , XXXXXXX   , XXXXXXX       , _______ ,
-    TG(4)   , KC_6       , KC_7         , KC_8         , KC_9         , KC_0    , KC_LEFT , KC_DOWN       , _______         , KC_RGHT        , XXXXXXX    , XXXXXXX     , _______   ,
-    _______ , KC_SLSH    , KC_DOT       , KC_COMM      , KC_MINS      , KC_BSPC , KC_DEL  , XXXXXXX       , XXXXXXX         , XXXXXXX        , XXXXXXX    , _______     , _______   ,
     _______ , _______    , _______      , _______      , _______      , _______ , _______ , _______       , _______         , _______        
   )
 };
